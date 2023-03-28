67a81f60c5ccbc5d961613fa9ec9fd5fa2d611f3
Jocelyn Putnam <98074087+JocelynPutnam@users.noreply.github.com>
2023-03-28 16:35:34 -0600

    Add files via upload

--- teamcontroller.php
+++ teamcontroller.php
@@ -3,12 +3,7 @@
 class Controller_Teamcontroller extends Controller_Template
 {
 	public $template = 'teamtemplate';
-	/**
-	 * The basic welcome message
-	 *
-	 * @access  public
-	 * @return  Response
-	 */
+
 	public function action_index()
 	{
 		//die('team index');
@@ -20,16 +15,14 @@ class Controller_Teamcontroller extends Controller_Template
 		$this->template->css = "style.css";
 	}
 
-	/**
-	 * A typical "Hello, Bob!" type example.  This uses a Presenter to
-	 * show how to use them.
-	 *
-	 * @access  public
-	 * @return  Response
-	 */
 	public function action_one()
 	{
 		return Response::forge(Presenter::forge('teamviews/one'));
+	}
+	
+	public function action_two()
+	{
+		return Response::forge(Presenter::forge('teamviews/two'));
 	}
 
 
