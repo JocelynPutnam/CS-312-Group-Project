4a40dc1ebdbd31978fd8e602255914b8d0839a73
MatthewMattoon <112028347+MatthewMattoon@users.noreply.github.com>
2023-03-30 17:35:53 -0600

    Add files via upload

--- teamviews/index.php
+++ teamviews/index.php
@@ -4,7 +4,30 @@
 <body>
 	
 <main>
-	<p>Home page!</p>
+
+<div> 
+
+<?php echo Asset::img('Logo.img.png'); ?>
+
+</div>
+<div>  
+<h2>Welcome to Goldfish Paint Design</h2>
+<h2>Where we strive for excellence in the modern age of Creativity!</h2>
+
+</div>
+<div>
+
+<p>Learn a little bit more about us! Visit the team!<p>
+<a href="./teamcontroller/one.php">About Us</a>
+    
+</div>
+<div>
+
+<p>Head to the Color Coordinate Generation page to create your own color coordinated table now!</p>
+<a href="./teamcontroller/two.php">Color Coordinate Generation</a>
+
+</div>
+
 </main>
 
 </body>
