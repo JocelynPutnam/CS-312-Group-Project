c28282ffd00de1c6c062083a65b82a645a7563b8
Alison <36714524+alisonsherwood123@users.noreply.github.com>
2023-03-30 16:24:27 -0600

    Add files via upload

--- teamviews/one.php
+++ teamviews/one.php
@@ -4,10 +4,18 @@
 <body>
 
 <main>
-	<p>About page!</p>
+	<h2>About Page!</h2>
+
+    <h3> Meet the team: </h3>
+
+    <img height="300" Width="225" alt="photo of Alison" src="Alison.jpeg">
+    <p> Hi! I'm Alison Sherwood, my pronouns are she/her, I'm a Junior studying business with a double 
+        concentration in Human Resource Managment and Innovation Management
+        along with a minor in Computer Science. I love spending time outdoors, reading
+        and crocheting. I also love to travel! </p>
+            
 </main>
 
 </body>
 
 </html>
-
