85dd5e4d33ecc69886bda98d3f909c131ef28abd
Alison <36714524+alisonsherwood123@users.noreply.github.com>
2023-03-30 19:49:29 -0600

    Add files via upload

--- teamviews/one.php
+++ teamviews/one.php
@@ -6,6 +6,8 @@
 <main>
 	<h2>About Page!</h2>
 
+    <a href="./index">Go Back to Home</a>
+
     <h3> Meet the team: </h3>
 
     <?php echo Asset::img('Alison.jpeg'); ?>
@@ -13,6 +15,8 @@
         concentration in Human Resource Managment and Innovation Management
         along with a minor in Computer Science. I love spending time outdoors, reading
         and crocheting. I also love to travel! A fun fact about me is that I have a pet gecko named Dawson.</p>
+
+        <br>
             
 </main>
 
