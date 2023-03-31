06610d7361f1b6def8ca2de888f499753b44cb60
jed312proj <129456309+jed312proj@users.noreply.github.com>
2023-03-31 16:34:31 -0600

    Update one.php

--- teamviews/one.php
+++ teamviews/one.php
@@ -22,15 +22,14 @@
 			<td><?php echo Asset::img('Jocelyn.jpeg'); ?></td>
 			<td><p> Hello! My name is Jocelyn Putnam and my pronouns are they/them. I'm an Art major with a minor in Computer science
 				and a passion for video games. Most of my freetime is spent making art or playing video games like Stardew Valley 
-				and LOZ: Breath of the Wild.</p></td>
 		</tr>
 		
 		<tr>					
-			<td><?php echo Asset::img('jed.png'); ?></td>
+			<td><?php echo Asset::img('Jed.PNG'); ?></td>
 			<td><p> Hello! I'm Jed, and my pronouns are he/him. I am currently taking CS312 Modern Web applications. In my free time, I try to stay active by going on hikes and running. <br> 
-			One fun fact about me is that even though I am not really ambidexterous, I have been trying to write with my non dominant hand recently, and I have seen significant improvement (it's slightly more legible now). </p></td>
+			One fun fact about me is that even though I am not really ambidexterous, I've been trying to write with my non dominant hand recently, and I have seen significant improvement (wrote the image on the left with my non-dominant hand). </p></td>
 		</tr>
-		
+				
 	</table>
 		
 		<br>
