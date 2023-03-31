cf42f37cc5cb80d220566baeba2258b14881f734
Alison <36714524+alisonsherwood123@users.noreply.github.com>
2023-03-30 19:33:48 -0600

    Add files via upload

--- teamviews/one.php
+++ teamviews/one.php
@@ -8,11 +8,11 @@
 
     <h3> Meet the team: </h3>
 
-    <img height="300" Width="225" alt="photo of Alison" src="Alison.jpeg">
+    <?php echo Asset::img('Alison.jpeg'); ?>
     <p> Hi! I'm Alison Sherwood, my pronouns are she/her, I'm a Junior studying business with a double 
         concentration in Human Resource Managment and Innovation Management
         along with a minor in Computer Science. I love spending time outdoors, reading
-        and crocheting. I also love to travel! </p>
+        and crocheting. I also love to travel! A fun fact about me is that I have a pet gecko named Dawson.</p>
             
 </main>
 
