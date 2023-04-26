6f4647a3cf12097b5f8b4a1639d81c10881427f3
Jocelyn Putnam <98074087+JocelynPutnam@users.noreply.github.com>
2023-04-26 15:06:34 -0600

    Add files via upload

---
+++ teamviews/error.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<main>
+<p>Color Coordinate Generation!</p>
+ 
+<p style="color: red; background-color: white">Please input a number of columns/rows between 1 and 26, and a color value between 1 and 10!</p>
+<form action="./two" method="post">
+Column/Row:<br> <input type="number" id="quantity" name="row" min="1" max="26" required>
+<br>
+Color:<br> <input type="number" id="quantity" name="color" min="1" max="10" required>
+<br>
+<input type="submit">
+</form>
+     
+</main>
+
+</body>
+
+</html>
